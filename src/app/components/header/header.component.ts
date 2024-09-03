caacaa353d1ab9d39fe109298e833aa1a023de87
danilovalerio <danilo_vs@hotmail.com>
2024-09-03 12:58:02 -0300

    adicao do button e icon

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatButtonModule, MatIconModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  public handlerOpenModal(): void {
+    alert('Adiciona tarefa');
+  }
 }
