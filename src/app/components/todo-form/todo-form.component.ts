81201e2ac2bf4507d74a12c674d4eefe9df1a1b9
danilovalerio <danilo_vs@hotmail.com>
2024-09-04 09:09:28 -0300

    todo-form - ts created

--- src/app/components/todo-form/todo-form.component.ts
+++ src/app/components/todo-form/todo-form.component.ts
@@ -1,11 +1,18 @@
 import { TodoSignalsService } from './../../services/todo-signals.service';
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
+import { title } from 'process';
 
 @Component({
   selector: 'app-todo-form',
@@ -26,4 +33,23 @@ export class TodoFormComponent {
   private todoSignalsService = inject(TodoSignalsService); //injeta como se fosse via  construtor
 
   public allTodos = this.todoSignalsService.todosState();
+
+  public todosForm = new FormGroup({
+    title: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    description: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+  });
+
+  public handleCreateNewTodo(): void {
+    if (this.todosForm.value && this.todosForm.valid) {
+      const title = String(this.todosForm.controls['title'].value);
+      const description = String(this.todosForm.controls['description'].value);
+      const id = this.allTodos.length > 0 ? this.allTodos.length + 1 : 1;
+      const done = false;
+
+      this.todoSignalsService.updateTodos({ id, title, description, done });
+    }
+  }
 }
