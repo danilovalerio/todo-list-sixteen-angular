460565c6c70a924ba1ce66d164a5668e798a7d37
danilovalerio <danilo_vs@hotmail.com>
2024-09-09 16:43:45 -0300

    implementacao do modal no header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { TodoFormComponent } from '../todo-form/todo-form.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatDialogModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  private dialogService = inject(MatDialog);
+
   public handlerOpenModal(): void {
-    alert('Adiciona tarefa');
+    this.dialogService.open(TodoFormComponent, {
+      width: '50vw',
+      maxHeight: '80vh',
+    });
   }
 }
