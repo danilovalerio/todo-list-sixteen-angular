7d3d9dc0c21e7d305fad67b87a410be4a090fcb3
danilovalerio <danilo_vs@hotmail.com>
2024-09-09 16:57:18 -0300

    imports que vms utilizar

--- src/app/components/todo-card/todo-card.component.ts
+++ src/app/components/todo-card/todo-card.component.ts
@@ -1,12 +1,23 @@
+import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-todo-card',
   standalone: true,
-  imports: [],
+  imports: [
+    NgFor,
+    NgIf,
+    NgTemplateOutlet,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTabsModule,
+  ],
   templateUrl: './todo-card.component.html',
-  styleUrl: './todo-card.component.scss'
+  styleUrl: './todo-card.component.scss',
 })
-export class TodoCardComponent {
-
-}
+export class TodoCardComponent {}
