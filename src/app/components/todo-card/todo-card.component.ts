49e0755443be776b272db44d6df3a7b390884b27
danilovalerio <danilo_vs@hotmail.com>
2024-09-10 08:40:43 -0300

    parte de controller do todoCard

--- src/app/components/todo-card/todo-card.component.ts
+++ src/app/components/todo-card/todo-card.component.ts
@@ -1,9 +1,12 @@
+import { Todo } from './../../models/model/todo.model';
 import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
+import { TodoSignalsService } from '../../services/todo-signals.service';
+import { TodoKeyLocalStorage } from '../../models/enum/todoKeyLocalStorage';
 
 @Component({
   selector: 'app-todo-card',
@@ -20,4 +23,44 @@ import { MatTabsModule } from '@angular/material/tabs';
   templateUrl: './todo-card.component.html',
   styleUrl: './todo-card.component.scss',
 })
-export class TodoCardComponent {}
+export class TodoCardComponent implements OnInit {
+  private todoSignalsService = inject(TodoSignalsService);
+  private todosSignal = this.todoSignalsService.todosState; //aqui contém o array de todos
+  //computed para usarmos o valores computados, que depende dos valores de outro signal, por exemplo o signal que está no state, assim atualiza automaticamente
+  private todosList = computed(() => this.todosSignal());
+
+  ngOnInit(): void {
+    this.getTodosInLocalStorage();
+  }
+
+  private getTodosInLocalStorage(): void {
+    const todosDatas = localStorage.getItem(
+      TodoKeyLocalStorage.TODO_LIST
+    ) as string;
+
+    todosDatas && this.todosSignal.set(JSON.parse(todosDatas));
+  }
+
+  private saveTodosInLocalStorage(): void {
+    this.todoSignalsService.saveTodosInLocalStorage();
+  }
+
+  public handleDoneTodo(todoId: number) {
+    if (todoId) {
+      this.todosSignal.set(
+        this.todosList().filter((item) => item.id == todoId)
+      );
+    }
+  }
+
+  public handleDeleteTodo(todo: Todo): void {
+    if (todo) {
+      const index = this.todosList().indexOf(todo);
+
+      if (index !== -1) {
+        this.todosSignal.set(this.todosList().splice(index, 1));
+        this.saveTodosInLocalStorage();
+      }
+    }
+  }
+}
